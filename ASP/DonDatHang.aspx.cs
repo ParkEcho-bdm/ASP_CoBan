4d738ac0b519e4183bd4a907096441b60f1e50e5
ParkEcho-bdm <buim8445@gmail.com>
2024-03-08 10:48:19 +0700

    first commit

---
+++ ASP/DonDatHang.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP
+{
+    public partial class DonDatHang : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddlBanh.Items.Add(new ListItem("Bún Bò ", "01"));
+                ddlBanh.Items.Add(new ListItem("Bánh kem 1000 lớp ", "02"));
+                ddlBanh.Items.Add(new ListItem("chu bin ", "03"));
+                ddlBanh.Items.Add(new ListItem("ăn gì cũng đc", "04"));
+            }
+        }
+
+        protected void ddlBanh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btThem_Click(object sender, EventArgs e)
+        {
+            bool checkExist = false;
+            char[] delim = { '(', ')' };
+            int soluong;
+            
+            for (int i = 0; i < lstBanh.Items.Count; i++)
+            {
+                string itemText = lstBanh.Items[i].Text;
+                if (itemText.Contains(ddlBanh.SelectedItem.Text))
+                { 
+                    string[] arr = itemText.Split(delim);
+                    soluong = int.Parse(txtSoLuong.Text) + int.Parse(arr[1]);
+                    lstBanh.Items[i].Text = $"{ddlBanh.SelectedItem.Text} ({soluong})";
+                    checkExist = true;
+                    break;
+                }
+            }
+            if (!checkExist)
+            {
+                string data = $"{ddlBanh.SelectedItem.Text} ({txtSoLuong.Text})";
+                lstBanh.Items.Add(data);
+            }
+        }
+
+        protected void btXoa_Click(object sender, ImageClickEventArgs e)
+        {
+            for (int i = lstBanh.Items.Count - 1; i >= 0; i--)
+            {
+                if (lstBanh.Items[i].Selected)
+                {
+                    lstBanh.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        protected void btIn_Click(object sender, EventArgs e)
+        {
+            string kq = "";
+            //b1.thu thap thong tin
+            kq += "<h2 class='text-center'> HOÁ ĐƠN ĐẶT HÀNG </h2>";
+            kq += "<div class='border border-primary p-2'>";
+            kq += "Khách hàng: <i>" + txtHoTen.Text + "</i><br>";
+            kq += "Địa chỉ: <i>" + txtDiaChi.Text + "</i><br>";
+            kq += "Mã số thuế: <i>" + txtMST.Text + "</i><br><br>";
+
+            kq += "<b>Đặt các loại bánh sau:</b>";
+            kq += "<table class='table table-bordered'>";
+            char[] delim = { '(', ')' };
+            foreach (ListItem item in lstBanh.Items)
+            {
+                string[] arr = item.Text.Split(delim);
+                kq += "<tr>";
+                kq += $"<td> {arr[0]} </td> <td> {arr[1]} </td>";
+                kq += "</tr>";
+            }
+
+            kq += "</table>";
+            kq += "</div>";
+
+
+            lbKetQua.Text = kq;
+        }
+    }
+}
---
+++ ASP/ThongTinCaNhan.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP
+{
+    public partial class ThongTinCaNhan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //khoi tao cho ddlTrinhDo
+                ddlTrinhDo.Items.Add(new ListItem("ĐẠi HỌC"));
+                ddlTrinhDo.Items.Add(new ListItem("Cao đẳng"));
+                //...
+
+                //khoi tao cho lstNgheNghiep
+                lstNgheNghiep.Items.Add(new ListItem("Công nhân"));
+                lstNgheNghiep.Items.Add(new ListItem("Lập trình viên"));
+                lstNgheNghiep.Items.Add(new ListItem("Kỹ sư"));
+                //...
+
+                //khoi tao cho chkListSoThich
+                chkListSoThich.Items.Add(new ListItem("$$$$"));
+                chkListSoThich.Items.Add(new ListItem("Xem fim"));
+                chkListSoThich.Items.Add(new ListItem("abcxyz"));
+                chkListSoThich.Items.Add(new ListItem("Ăn"));
+     
+            }
+
+        }
+
+        protected void btGui_Click(object sender, EventArgs e)
+        {
+            string kq = "";
+    
+            kq += "<h2 class='mt-3'>Thông tin đăng ký của bạn</h2>";
+            kq += "<ul>";
+
+            kq += $"<li>Họ tên: {txtHoTen.Text}</li>";
+            kq += string.Format("<li> Ngày sinh: {0} </li>", txtNgaySinh.Text);
+            if (rdNam.Checked)
+            {
+                kq += $"<li> Giới tính: {rdNam.Text} </li>";
+            }
+            else
+            {
+                kq += $"<li> Giới tính: {rdNu.Text} </li>";
+            }
+
+            kq += $"<li>Trình độ: {ddlTrinhDo.SelectedItem.Text}</li>";
+            kq += $"<li>Nghề nghiệp: {lstNgheNghiep.SelectedItem.Text}</li>";
+
+            if (FHinh.HasFile)
+            {
+                string path = Server.MapPath("~/Uploads");
+                FHinh.SaveAs(path + "/" + FHinh.FileName);
+                kq += $"<li>Ảnh: <img src='Uploads/{FHinh.FileName}'> </li>";
+            }
+
+            string sothich = "";
+            foreach (ListItem x in chkListSoThich.Items)
+            {
+                if (x.Selected)
+                {
+                    sothich += x.Text + ";";
+                }
+            }
+
+            kq += $"<li>Sở thích: {sothich}</li>";
+
+            kq += "<ul>";
+            lbKetQua.Text = kq;
+
+        }
+    }
+}
